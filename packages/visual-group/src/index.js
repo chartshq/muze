f9e4efd66b99360b147c8367ded29ca616249b87
Rousan Ali <rousanali786@gmail.com>
2018-08-17 18:18:50 +0530

    Change named export to default export

--- packages/visual-group/src/index.js
+++ packages/visual-group/src/index.js
@@ -1,4 +1,4 @@
 export { VisualGroup } from './visual-group';
 export { SimpleGroup } from './simple-group';
 export { ComposedVars, SimpleVariable } from './variable';
-export { ValueMatrix } from './visual-group/value-matrix';
+export { default as ValueMatrix } from './visual-group/value-matrix';
