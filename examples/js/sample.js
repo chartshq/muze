205e5175cb0c9d2cf16f45611fc10c012a5cda0e
Mridul Maheria <Mridul@fusioncharts.com>
2018-08-21 19:03:31 +0530

    CDC-6: Changed title color as per design and default legend position

--- examples/js/sample.js
+++ examples/js/sample.js
@@ -1,151 +1,127 @@
+/* eslint-disable */
+
 (function () {
     let env = muze();
     let DataModel = muze.DataModel,
         share = muze.operators.share,
         html = muze.operators.html,
         actionModel = muze.ActionModel;
-    layerFactory = muze.layerFactory;
+        
+        let layerFactory = muze.layerFactory;
     const SpawnableSideEffect = muze.SideEffects.SpawnableSideEffect;
 
-    d3.json('../../data/cars.json', (data) => {
-        const jsonData = data,
-            // schema = [{
-            //     name: 'date',
-            //     type: 'dimension',
-            //     subtype: 'temporal',
-            //     format: '%Y/%m/%d'
-            // }, {
-            //     name: 'temp_max',
-            //     type: 'measure'
-            // }];
-            schema = [{
-                name: 'Name',
-                type: 'dimension'
-            },
-            {
-                name: 'Maker',
-                type: 'dimension'
-            },
-            {
-                name: 'Miles_per_Gallon',
-                type: 'measure'
-            },
+	d3.json('../data/cars.json', (data) => {
+		const jsonData = data,
+			schema = [{
+					name: 'Name',
+					type: 'dimension'
+				},
+				{
+					name: 'Maker',
+					type: 'dimension'
+				},
+				{
+					name: 'Miles_per_Gallon',
+					type: 'measure'
+				},
 
-            {
-                name: 'Displacement',
-                type: 'measure'
-            },
-            {
-                name: 'Horsepower',
-                type: 'measure'
-            },
-            {
-                name: 'Weight_in_lbs',
-                type: 'measure',
-            },
-            {
-                name: 'Acceleration',
-                type: 'measure',
-            },
-            {
-                name: 'Origin',
-                type: 'dimension'
-            },
-            {
-                name: 'Cylinders',
-                type: 'dimension'
-            },
-            {
-                name: 'Year',
-                type: 'dimension',
-                // subtype: 'temporal',
-                // format: '%Y-%m-%d'
-            },
-            ];
+				{
+					name: 'Displacement',
+					type: 'measure'
+				},
+				{
+					name: 'Horsepower',
+					type: 'measure'
+				},
+				{
+					name: 'Weight_in_lbs',
+					type: 'measure',
+				},
+				{
+					name: 'Acceleration',
+					type: 'measure'
+				},
+				{
+					name: 'Origin',
+					type: 'dimension'
+				},
+				{
+					name: 'Cylinders',
+					type: 'dimension'
+				},
+				{
+					name: 'Year',
+					type: 'dimension',
+					subtype: 'temporal',
+					format: '%Y-%m-%d'
+				},
 
-        layerFactory.composeLayers('heatMapText', [
-            {
-                name: 'bar',
-                mark: 'bar',
-                encoding: {
-                    y: 'heatMapText.encoding.y',
-                    x: 'heatMapText.encoding.x',
-                },
-            },
-            {
-                name: 'text',
-                mark: 'text',
-                encoding: {
-                    x: 'heatMapText.encoding.x',
-                    y: 'heatMapText.encoding.y',
-                    text: 'heatMapText.encoding.text',
-                    color: 'heatMapText.encoding.color',
-                },
-            },
-        ]);
-        let rootData = new DataModel(jsonData, schema);
+			];
+		let rootData = new DataModel(jsonData, schema);
 
-        rootData = rootData.groupBy(['Year', 'Origin', 'Maker'], {
-            Horsepower: 'mean',
-            Acceleration: 'mean'
-        });
-        env = env.data(rootData).minUnitHeight(40).minUnitWidth(40);
-        const mountPoint = document.getElementById('chart');
-        window.canvas = env.canvas();
-        let rows = ['Acceleration'],
-            columns = ['Horsepower'];
+		rootData = rootData.groupBy([ 'Year','Origin'], {
+			Horsepower: 'mean',
+			Acceleration: 'mean'
+		});
 
-        canvas = canvas
-            .rows(rows)
+		env = env.data(rootData).minUnitHeight(40).minUnitWidth(40);
+		let mountPoint = document.getElementById('chart');
+		window.canvas = env.canvas();
+		let canvas2 = env.canvas();
+		let canvas3 = env.canvas();
+		let rows = [[ 'Acceleration']],
+			columns = ['Horsepower'];
+		canvas = canvas
+			.rows(rows)
             .columns(columns)
+            .color('Origin')
             .data(rootData)
-            .width(400)
-            .height(500)
-            .config({
-                gridLines: {
-                    y: {
-                        show: true
-                    }
-                },
-                facetConfig: {
-                    rows: {
-                        verticalAlign: 'middle',
-                    }
-                },
-                border: {
-                    width: 2,
-                },
-                axes: {
-                    x: {
-                        showAxisName: true,
-                        showInnerTicks: true,
-
-                    },
-                    y: {
-                        showAxisName: true,
-                        // interpolator: 'log',
-                        // exponent: 2,
-                        // base: 10,
-
-                    },
+            .layers({
+                'Horsepower':{
+                    mark: 'point',
                 }
             })
-            .color({
-                field: 'Origin'
-            })
-            .legend({
-                align: 'horizontal',
-
-                position: 'top',
-                item: {
-                    text: {
-                        position: 'left'
+			.width(900)
+            .height(600)
+            .config({
+                border:{
+                    width: 2,
+                    showRowBorders: false,
+                    showColBorders:false,
+                    showValueBorders: {
+                        top: false,
+                        bottom: true,
+                        left: true,
+                        right: false
                     }
                 },
+                axes:{
+                        x:{
+                            showAxisName: true,
+                          
+                        
+                    }, y:{
+                        showAxisName: true,
+                        // name: 'Acceleration per year',
+                        axisNamePadding: 12
+                    }
+                }
             })
 
-                        .title('The Muze Project', { position: 'top', align: 'left', })
-                        .subtitle('Composable visualisations with a data first approach', { position: 'top', align: 'left' })
-                        .mount(document.getElementsByTagName('body')[0]);
-    });
-}());
+        canvas.legend({
+            align:'vertical',
+            // ,
+            item:{
+                text:{
+                    position:'right'
+                }
+            }, 
+            steps: 6
+        })
+    
+        .title('The Muze Project', { position: "top", align: "left",  })
+		.subtitle('Composable visualisations with a data first approach', { position: "top", align: "left" })
+		.mount(document.getElementsByTagName('body')[0]);
+	})
+
+})()
