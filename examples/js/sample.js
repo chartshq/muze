993fd274874f69f3162ff158cdd21ef67205491b
Ranajit Banerjee <ranajit.banerjee@fusioncharts.com>
2018-09-05 19:31:33 +0530

    CDC-45: Add dual axis sample

--- examples/js/sample.js
+++ examples/js/sample.js
@@ -1,108 +1,82 @@
-/* eslint-disable */
+let env = muze();
+let DataModel = muze.DataModel,
+    share = muze.Operators.share,
+    html = muze.Operators.html,
+    actionModel = muze.ActionModel;
+const SpawnableSideEffect = muze.SideEffects.SpawnableSideEffect;
 
-(function () {
-    let env = muze();
-    let DataModel = muze.DataModel,
-        share = muze.operators.share,
-        html = muze.operators.html,
-        actionModel = muze.ActionModel;
-    const SpawnableSideEffect = muze.SideEffects.SpawnableSideEffect;
+d3.json('./data/cars.json', (data) => {
+    let jsonData = data,
+        schema = [{
+            name: 'Name',
+            type: 'dimension'
+        },
+        {
+            name: 'Maker',
+            type: 'dimension'
+        },
+        {
+            name: 'Miles_per_Gallon',
+            type: 'measure'
+        },
 
+        {
+            name: 'Displacement',
+            type: 'measure'
+        },
+        {
+            name: 'Horsepower',
+            type: 'measure'
+        },
+        {
+            name: 'Weight_in_lbs',
+            type: 'measure'
+        },
+        {
+            name: 'Acceleration',
+            type: 'measure',
+            defAggFn: 'avg'
+        },
+        {
+            name: 'Origin',
+            type: 'dimension'
+        },
+        {
+            name: 'Cylinders',
+            type: 'dimension'
+        },
+        {
+            name: 'Year',
+            type: 'dimension'
+            // subtype: 'temporal',
+            // format: '%Y-%m-%d'
+        }
 
+        ];
+    const rootData = new DataModel(jsonData, schema);
 
-    d3.json('../data/cars.json', (data) => {
-        const jsonData = data,
-            schema = [{
-                name: 'Name',
-                type: 'dimension'
-            },
-            {
-                name: 'Maker',
-                type: 'dimension'
-            },
-            {
-                name: 'Miles_per_Gallon',
-                type: 'measure'
-            },
+    // rootData = rootData.groupBy(['Year'], {
+    //     Horsepower: 'mean',
+    //     Acceleration: 'mean'
+    // });
 
-            {
-                name: 'Displacement',
-                type: 'measure'
-            },
-            {
-                name: 'Horsepower',
-                type: 'measure'
-            },
-            {
-                name: 'Weight_in_lbs',
-                type: 'measure',
-            },
-            {
-                name: 'Acceleration',
-                type: 'measure',
-                defAggFn: 'avg'
-            },
-            {
-                name: 'Origin',
-                type: 'dimension'
-            },
-            {
-                name: 'Cylinders',
-                type: 'dimension'
-            },
-            {
-                name: 'Year',
-                type: 'dimension',
-                // subtype: 'temporal',
-                // format: '%Y-%m-%d'
-            },
-
-            ];
-        let rootData = new DataModel(jsonData, schema);
-
-        // rootData = rootData.groupBy(['Year'], {
-        //     Horsepower: 'mean',
-        //     Acceleration: 'mean'
-        // });
-
-        env = env.data(rootData).minUnitHeight(40).minUnitWidth(40);
-        let mountPoint = document.getElementById('chart');
-        window.canvas = env.canvas();
-        let rows = ['Acceleration'],
-            columns = ['Displacement'];
-        // rootData = rootData.groupBy(['Maker']);
-        // rootData = rootData.sort([['Acceleration', 'ASC']]);
-        canvas = canvas
-            .rows(rows)
-            .columns(columns)
-            .data(rootData)
-            .width(1200)
-            .height(800)
-            .detail(['Maker'])
-            .color('Origin')
-            .layers([{
-                mark: 'point'
-            }])
-            .config({
-                groupBy: {
-                    disabled: true
-                },
-                border: {
-                    width: 2,
-                },
-                axes: {
-                    x: {
-                        showAxisName: true,
-                        axisNamePadding: 20,
-                    }, y: {
-                        showAxisName: true,
-                        axisNamePadding: 20,
-                    }
-                }
-            })
-            .title('The Muze Project', { position: "top", align: "left", })
-            .subtitle('Composable visualisations with a data first approach', { position: "top", align: "left" })
-            .mount(document.getElementsByTagName('body')[0]);
-    })
-
-})()
+    env = env.data(rootData).minUnitHeight(40).minUnitWidth(40);
+    const mountPoint = document.getElementById('chart');
+    window.canvas = env.canvas();
+    canvas = canvas
+        .rows([['Acceleration'], ['Displacement']])
+        .columns(['Year'])
+        .data(rootData)
+        .minUnitHeight(100)
+        .minUnitWidth(100)
+        .height(600)
+        .width(800)
+        .layers([{
+            mark: 'line'
+        }])
+        // .detail(['Name']) // Show all the data point
+        .color('Origin') // Color the points using cylinders
+        .title('SPLOM', { position: 'top', align: 'right' })
+        .subtitle('Correlation matrix of all the measures', { position: 'top', align: 'right' })
+        .mount(mountPoint);
+});
