ee7e70caf01b6856f080dfceed5985ce14d4ece1
Sanjay Mahto <mahto.sanjay41@gmail.com>
2019-02-06 19:19:14 +0530

    example js file modified.

--- examples/js/stackBarChart.js
+++ examples/js/stackBarChart.js
@@ -12,7 +12,6 @@ d3.json('../data/cars.json', (data) => {
         name: 'Miles_per_Gallon',
         type: 'measure'
     },
-
     {
         name: 'Displacement',
         type: 'measure'
@@ -43,37 +42,33 @@ d3.json('../data/cars.json', (data) => {
         subtype: 'temporal',
         format: '%Y-%m-%d'
     }
-
     ];
-
     const DataModel = muze.DataModel;
     const rootData = new DataModel(data, schema);
-
     const env = muze();
     const canvas = env.canvas();
-
     canvas
-  		.rows(['Acceleration']) // CountVehicle goes in y axis
-          .columns(['Maker']) // Cylinders goes in x-axis
-          .color({
-              field: 'Acceleration',
-              stops: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 56, 73, 45, 67, 48, 120, 130, 110, 115, 125, 567, 234, 123, 456, 67, 90, 150, 670, 700]
-          })
-
+        .rows(['Acceleration']) // Acceleration goes in y axis
+        .columns(['Maker']) // Maker goes in x-axis
+        .color({
+            field: 'Acceleration',
+            stops: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 56, 73, 45, 67, 48, 120, 130, 110,
+                115, 125, 567, 234, 123, 456, 67, 90, 150, 670, 700]
+        })
         .data(rootData)
-  		.layers({ // Draw a bar plot, by default stack transform is used
-        	Acceleration: {
-            	mark: 'bar'
-        }
-  })
-                    .config({
-                        legend: {
-                            position: 'bottom'
-                        }
-                    })
-      	.width(600)
-      	.height(500)
-  		.title('Stacked bar chart', { position: 'top', align: 'right' })
-  		.subtitle('Count of cars per cylinder per origin', { position: 'top', align: 'right' })
-          .mount('#chart'); // Set the chart mount point
+        .layers({ // Draw a bar plot, by default stack transform is used
+            Acceleration: {
+                mark: 'bar'
+            }
+        })
+        .config({
+            legend: {
+                position: 'bottom'
+            }
+        })
+        .width(600)
+        .height(500)
+        .title('Stacked bar chart', { position: 'top', align: 'right' })
+        .subtitle('Count of cars per cylinder per origin', { position: 'top', align: 'right' })
+        .mount('#chart'); // Set the chart mount point
 });
