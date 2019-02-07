3ede3bd39ff14acaf2f0abb04096bb833751ca43
Sanjay Mahto <mahto.sanjay41@gmail.com>
2019-02-07 17:06:51 +0530

    rectified the issue for updating title and subtitle.

--- examples/js/stackBarChart.js
+++ examples/js/stackBarChart.js
@@ -81,7 +81,7 @@ d3.json('../data/cars.json', (data) => {
           .mount('#chart'); // Set the chart mount point
 
     setTimeout(() => {
-        canvas.title('Updated Title')
-              .subtitle('Updated Subtitle');
-    }, 2500);
+        canvas.title('')
+        .subtitle('');
+    }, 5000);
 });
