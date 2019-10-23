1d16e37eeb63034c56e959a4ce02ffcc31962a4b
adarshlilha <adarshlilha@gmail.com>
2019-10-24 00:15:35 +0530

    MZ-63: Refactor code

--- packages/muze-firebolt/src/side-effects/plot-highlighter/strategy.js
+++ packages/muze-firebolt/src/side-effects/plot-highlighter/strategy.js
@@ -1,21 +1,4 @@
-import { getArrayDiff } from 'muze-utils';
-
-/**
- * @private
- * @param {Object} set Points set
- * @param {Array.<number>} selectedPointsId array of id of selected points
- * @return {Object} Returns the set with the selected points removed
- */
-const getFormattedSet = (set, selectedPointsId) => {
-    const formattedSet = getArrayDiff(set.uids, selectedPointsId);
-    return {
-        ...set,
-        ...{
-            uids: formattedSet,
-            length: formattedSet.length
-        }
-    };
-};
+import { getFormattedSet } from './helper';
 
 const fadeFn = (set, context, strategy) => {
     const {
