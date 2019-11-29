9e50413302ced7e8b4b7eaa33bd5f248a48c1fa6
swamu <swati@charts.com>
2019-11-29 18:00:30 +0530

    legend unnecessary change

--- examples/js/tooltips/simple-bar.js
+++ examples/js/tooltips/simple-bar.js
@@ -1,54 +1,64 @@
-var env = window.muze();
-var DataModel = window.muze.DataModel;
+/* eslint-disable */
+const env = muze();
+const DataModel = muze.DataModel;
 
-d3.json('/data/cars-with-null.json', function (data) {
-    var schema = [{
+d3.json('../../data/cars.json', (data) => {
+    let jsonData = data;
+    const schema = [{
         name: 'Name',
         type: 'dimension'
-    }, {
+    },
+    {
         name: 'Maker',
         type: 'dimension'
-    }, {
+    },
+    {
         name: 'Miles_per_Gallon',
         type: 'measure'
-    }, {
+    },
+
+    {
         name: 'Displacement',
         type: 'measure'
-    }, {
+    },
+    {
         name: 'Horsepower',
         type: 'measure'
-    }, {
+    },
+    {
         name: 'Weight_in_lbs',
         type: 'measure'
-    }, {
+    },
+    {
         name: 'Acceleration',
         type: 'measure'
-    }, {
+    },
+    {
         name: 'Origin',
         type: 'dimension'
-    }, {
+    },
+    {
         name: 'Cylinders',
         type: 'dimension'
-    }, {
+    },
+    {
         name: 'Year',
         type: 'dimension'
-    }];
-    // Create an instance of DataModel using the data and schema.
-    var rootData = new DataModel(data, schema);
+    }
+    ];
+    var rootData = new DataModel(data, schema, { dataFormat: "FlatJSON" });
 
-    // Get a canvas instance from Muze where the chart will be rendered.
-    var rows = [['Acceleration']],
-        columns = [['Year']];
-
-    env.canvas().rows(rows).columns(columns).data(rootData).layers([{
-        mark: 'bar'
-    }]).width(800).config({
-        legend: {
-            position: 'bottom'
-        } }).height(600).columns(['Acceleration']).rows(['Year']).layers([{
-        mark: 'point'
-    }]).shape('Cylinders').mount('#chart').once('canvas.animationend').then(function (client) {
-        var element = document.getElementById('chart');
-        element.classList.add('animateon');
-    });
+    var canvas = env.canvas();
+    var rows = ["Acceleration"];
+    var columns = ["Origin"];
+    canvas
+        .mount('#chart1')
+        .width(600)
+        .data(rootData)
+        .rows(rows)
+        .height(500)
+        .columns(columns)
+        .color({
+            field: 'Maker'
+        })
 });
