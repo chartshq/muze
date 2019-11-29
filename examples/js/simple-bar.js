6eb40e5a456138c6f8002f5480568d4189d1229c
Ujjal Kumar Dutta <ujjal@charts.com>
2019-11-29 13:46:27 +0530

    Legend Marker Positioning

--- examples/js/simple-bar.js
+++ examples/js/simple-bar.js
@@ -1,10 +1,6 @@
-(function () {
-    let env = window.muze();
-    const DataModel = window.muze.DataModel;
-
-    d3.json('../data/cars.json', (data) => {
-        let jsonData = data,
-		    schema = [{
+d3.json('../data/cars.json', (data) => {
+    // load data and schema from url
+    const schema = [{
         name: 'Name',
         type: 'dimension'
     }, {
@@ -12,19 +8,24 @@
         type: 'dimension'
     }, {
         name: 'Miles_per_Gallon',
-        type: 'measure'
+        type: 'measure',
+        defAggFn: 'avg'
     }, {
         name: 'Displacement',
-        type: 'measure'
+        type: 'measure',
+        defAggFn: 'max'
     }, {
         name: 'Horsepower',
-        type: 'measure'
+        type: 'measure',
+        defAggFn: 'avg'
     }, {
         name: 'Weight_in_lbs',
-        type: 'measure'
+        type: 'measure',
+        defAggFn: 'min'
     }, {
         name: 'Acceleration',
-        type: 'measure'
+        type: 'measure',
+        defAggFn: 'avg'
     }, {
         name: 'Origin',
         type: 'dimension'
@@ -33,42 +34,45 @@
         type: 'dimension'
     }, {
         name: 'Year',
-        type: 'dimension'
-			// {dimType}
-			// subtype: 'temporal',
-			// format: '%Y-%m-%d'
+        type: 'dimension',
+        subtype: 'temporal',
+        format: '%Y-%m-%d'
     }];
-        let rootData = new DataModel(jsonData, schema);
-        rootData = rootData.calculateVariable({
-            name: 'date',
-            type: 'dimension',
-            subtype: 'temporal',
-            format: '%Y-%m-%d'
-        }, ['Year', function (d) {
-            return d;
-        }]);
 
-        env = env.data(rootData).minUnitHeight(40).minUnitWidth(40);
-        const mountPoint = document.getElementById('chart');
-        window.canvas = env.canvas();
-        let rows = ['Cylinders', 'Horsepower'],
-		    columns = ['Origin', 'Year'];
-        canvas = canvas.rows(rows).columns(columns).height(800).color('Origin')
-		// {initProps}
-		.mount(mountPoint);
+    // Create an instance of DataModel using the data and schema.
+    let dm = new muze.DataModel(data, schema);
+    // Sort the data. Before sorting groupBy needs to be performed. As grouping after sorting changes the order.
+    // When sorting is applied, internal grouping of data inside muze has to be turned off.
+    const filterMaker = ['bmw', 'honda', 'ford', 'volvo', 'volkswagen', 'audi', 'renault', 'toyota', 'dodge', 'chevrolet', 'plymouth'];
+    dm = dm.groupBy(['Maker']).sort([['Acceleration', 'ASC']]);
+    dm = dm.select(fields => filterMaker.indexOf(fields.Maker.value) > -1);
+    // Create an environment for future rendering
+    const env = window.muze();
+    // Create an instance of canvas which houses the visualization
+    const canvas = env.canvas();
 
-        canvas.once('canvas.animationend').then((client) => {
-            canvas.config({
-                axes: {
-                    x: {
-                        name: 'Long long long long long long long long long long axis name'
-                    }
-                }
-            });
-            canvas.once('canvas.animationend').then((client) => {
-                const element = document.getElementById('chart');
-                element.classList.add('animateon');
-            });
-        });
+    canvas.rows(['Maker']) // Year goes in X axis
+    .columns(['Acceleration']) // Acceleration goes in Y axis
+    .data(dm).color({
+        field: 'Acceleration', // A measure in color encoding channel creates gradient legend
+        stops: 10, // 3 stops with interpolated value
+        range: ['#eaeaea', '#258e47'] // range could be either set of color or predefined palletes
+    }).config({
+        autoGroupBy: { // Turn off internal grouping of data because data has order wich needs to be maintained
+            disabled: true
+        },
+        legend: {
+            position: 'bottom'
+        }
+    }).width(600) // Set the chart width
+    .height(400) // Set the chart height
+    .title('The car acceleration respective to origin', { position: 'bottom', align: 'center' })
+    // .title('Bar chart with gradient legend', { position: 'bottom', align: 'right', })
+    .subtitle('Change of acceleration over the years colored with Horsepower', { position: 'bottom', align: 'right' }).mount('#chart'); // Render on a dom element
+
+    canvas.once('canvas.animationend').then((client) => {
+        const element = document.getElementById('chart');
+        element.classList.add('animateon');
     });
-}());
+});
+
