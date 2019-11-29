5327d94f202743ff7924aad7e4d08a3c511f0eec
Ujjal Kumar Dutta <ujjal@charts.com>
2019-11-29 12:59:47 +0530

    Legend Click Sort Error

--- examples/js/simple-bar.js
+++ examples/js/simple-bar.js
@@ -1,143 +1,74 @@
-d3.json('../data/iris.cleared.json', (data) => {
-    // load data and schema from url
-    const schema = [{
-        name: 'organ',
+(function () {
+    let env = window.muze();
+    const DataModel = window.muze.DataModel;
+
+    d3.json('../data/cars.json', (data) => {
+        let jsonData = data,
+		    schema = [{
+        name: 'Name',
         type: 'dimension'
     }, {
-        name: 'minValue',
+        name: 'Maker',
+        type: 'dimension'
+    }, {
+        name: 'Miles_per_Gallon',
         type: 'measure'
     }, {
-        name: 'meanValue',
+        name: 'Displacement',
         type: 'measure'
     }, {
-        name: 'maxValue',
+        name: 'Horsepower',
         type: 'measure'
     }, {
-        name: 'quarter',
+        name: 'Weight_in_lbs',
         type: 'measure'
     }, {
-        name: 'thirdQuarter',
+        name: 'Acceleration',
         type: 'measure'
+    }, {
+        name: 'Origin',
+        type: 'dimension'
+    }, {
+        name: 'Cylinders',
+        type: 'dimension'
+    }, {
+        name: 'Year',
+        type: 'dimension'
+			// {dimType}
+			// subtype: 'temporal',
+			// format: '%Y-%m-%d'
     }];
-    const layers = [{
-        name: 'maxTick',
-        mark: 'tick',
-        className: 'boxTicks',
-        encoding: {
-            y: 'boxMark.encoding.maxValue',
-            x: 'boxMark.encoding.x'
-        },
-        interactive: false
-    }, {
-        name: 'upperTick',
-        className: 'upper-tick',
-        mark: 'tick',
-        encoding: {
-            y: 'boxMark.encoding.quarter',
-            x: 'boxMark.encoding.x',
-            y0: 'boxMark.encoding.minValue'
-        },
-        interactive: false
-    }, {
-        name: 'upperBand',
-        mark: 'bar',
-        className: 'upperBand',
-        encoding: {
-            y: 'boxMark.encoding.thirdQuarter',
-            x: 'boxMark.encoding.x',
-            y0: 'boxMark.encoding.meanValue',
-            color: 'boxMark.encoding.color'
-        },
-        transform: {
-            type: 'identity'
-        }
-    }, {
-        name: 'meanTick',
-        mark: 'tick',
-        className: 'boxTicks',
-        encoding: {
-            y: 'boxMark.encoding.meanValue',
-            x: 'boxMark.encoding.x'
-        },
-        interactive: false
-    }, {
-        name: 'lowerBand',
-        mark: 'bar',
-        className: 'lowerBand',
-        encoding: {
-            y0: 'boxMark.encoding.meanValue',
-            x: 'boxMark.encoding.x',
-            y: 'boxMark.encoding.quarter',
-            color: 'boxMark.encoding.color'
-        },
-        transform: {
-            type: 'identity'
-        }
-    }, {
-        name: 'lowerTick',
-        mark: 'tick',
-        className: 'boxTicks',
-        encoding: {
-            y: 'boxMark.encoding.maxValue',
-            x: 'boxMark.encoding.x',
-            y0: 'boxMark.encoding.thirdQuarter'
-        },
-        interactive: false
-    }, {
-        name: 'minTick',
-        mark: 'tick',
-        className: 'boxTicks',
-        encoding: {
-            y: 'boxMark.encoding.minValue',
-            x: 'boxMark.encoding.x'
-        },
-        interactive: false
-    }];
+        let rootData = new DataModel(jsonData, schema);
+        rootData = rootData.calculateVariable({
+            name: 'date',
+            type: 'dimension',
+            subtype: 'temporal',
+            format: '%Y-%m-%d'
+        }, ['Year', function (d) {
+            return d;
+        }]);
 
-    const DataModel = window.muze.DataModel;
-    const rootData = new DataModel(data, schema);
-    // Registry for user defined layers
-    const layerFactory = muze.layerFactory;
-    // Compose share operator for plotting multiple variable in one Y-axis
-    const share = muze.Operators.share;
+        env = env.data(rootData).minUnitHeight(40).minUnitWidth(40);
+        const mountPoint = document.getElementById('chart');
+        window.canvas = env.canvas();
+        let rows = ['Cylinders', 'Horsepower'],
+		    columns = ['Origin', 'Year'];
+        canvas = canvas.rows(rows).columns(columns).height(800).color('Origin')
+		// {initProps}
+		.mount(mountPoint);
 
-    // Create a global environment to share common configs across charts
-    const env = window.muze();
-    // Set height, width and data to env, so that every instance of canvas which gets created from the environment
-    // receives this
-    env.height(400).width(400).data(rootData);
-
-    const canvas = env.canvas();
-
-    // Use the custom layer definition to register a new layer and name it boxMark
-    layerFactory.composeLayers('boxMark', layers);
-    let sharedField = void 0;
-    // Create a combined field which gets plotted in the Y-axis. Value of all those variables will be passed
-    // to layers
-    const columns = [sharedField = share('minValue', 'meanValue', 'maxValue', 'quarter', 'thirdQuarter')];
-    const rows = ['organ'];
-
-    canvas.rows(columns).columns(rows).color('organ').config({
-        axes: {
-            y: {
-                showAxisName: true,
-                name: 'Measure'
-            }
-        }
-    }).layers([{
-        mark: 'boxMark',
-        encoding: { // Map the encoding with variables. These custom encodings are used in the composite layers.
-            minValue: 'minValue',
-            meanValue: 'meanValue',
-            x: 'organ',
-            maxValue: 'maxValue',
-            quarter: 'quarter',
-            thirdQuarter: 'thirdQuarter'
-        }
-    }]).subtitle('The car acceleration respective to origin', { position: 'bottom', align: 'center' }).mount(document.getElementById('chart'));
-
-    canvas.once('canvas.animationend').then((client) => {
-        const element = document.getElementById('chart');
-        element.classList.add('animateon');
+        canvas.once('canvas.animationend').then((client) => {
+            canvas.config({
+                axes: {
+                    x: {
+                        name: 'Long long long long long long long long long long axis name'
+                    }
+                }
+            });
+            canvas.once('canvas.animationend').then((client) => {
+                const element = document.getElementById('chart');
+                element.classList.add('animateon');
+            });
+        });
     });
-});
+}());
